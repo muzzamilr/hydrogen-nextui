34e8f215e37d185add44f5e70678b51865d2064a
muzzamilr <mmuzzamil90@gmail.com>
2024-01-18 21:51:12 +0500

    init

--- app/root.tsx
+++ app/root.tsx
@@ -1,176 +1,183 @@
-import {useNonce} from '@shopify/hydrogen';
+import { useNonce } from "@shopify/hydrogen";
 import {
-  defer,
-  type SerializeFrom,
-  type LoaderFunctionArgs,
-} from '@shopify/remix-oxygen';
+	defer,
+	type SerializeFrom,
+	type LoaderFunctionArgs,
+} from "@shopify/remix-oxygen";
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  LiveReload,
-  useMatches,
-  useRouteError,
-  useLoaderData,
-  ScrollRestoration,
-  isRouteErrorResponse,
-  type ShouldRevalidateFunction,
-} from '@remix-run/react';
-import type {CustomerAccessToken} from '@shopify/hydrogen/storefront-api-types';
-import favicon from '../public/favicon.svg';
-import resetStyles from './styles/reset.css';
-import appStyles from './styles/app.css';
-import {Layout} from '~/components/Layout';
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	LiveReload,
+	useMatches,
+	useRouteError,
+	useLoaderData,
+	ScrollRestoration,
+	isRouteErrorResponse,
+	type ShouldRevalidateFunction,
+} from "@remix-run/react";
+import type { CustomerAccessToken } from "@shopify/hydrogen/storefront-api-types";
+import favicon from "../public/favicon.svg";
+import resetStyles from "./styles/reset.css";
+import appStyles from "./styles/app.css";
+import tailwidStyles from "./styles/tailwind.css";
+import { Layout } from "~/components/Layout";
+import { NextUIProvider } from "@nextui-org/react";
 
 /**
  * This is important to avoid re-fetching root queries on sub-navigations
  */
 export const shouldRevalidate: ShouldRevalidateFunction = ({
-  formMethod,
-  currentUrl,
-  nextUrl,
+	formMethod,
+	currentUrl,
+	nextUrl,
 }) => {
-  // revalidate when a mutation is performed e.g add to cart, login...
-  if (formMethod && formMethod !== 'GET') {
-    return true;
-  }
+	// revalidate when a mutation is performed e.g add to cart, login...
+	if (formMethod && formMethod !== "GET") {
+		return true;
+	}
 
-  // revalidate when manually revalidating via useRevalidator
-  if (currentUrl.toString() === nextUrl.toString()) {
-    return true;
-  }
+	// revalidate when manually revalidating via useRevalidator
+	if (currentUrl.toString() === nextUrl.toString()) {
+		return true;
+	}
 
-  return false;
+	return false;
 };
 
 export function links() {
-  return [
-    {rel: 'stylesheet', href: resetStyles},
-    {rel: 'stylesheet', href: appStyles},
-    {
-      rel: 'preconnect',
-      href: 'https://cdn.shopify.com',
-    },
-    {
-      rel: 'preconnect',
-      href: 'https://shop.app',
-    },
-    {rel: 'icon', type: 'image/svg+xml', href: favicon},
-  ];
+	return [
+		{ rel: "stylesheet", href: resetStyles },
+		{ rel: "stylesheet", href: appStyles },
+		{ rel: "stylesheet", href: tailwidStyles },
+		{
+			rel: "preconnect",
+			href: "https://cdn.shopify.com",
+		},
+		{
+			rel: "preconnect",
+			href: "https://shop.app",
+		},
+		{ rel: "icon", type: "image/svg+xml", href: favicon },
+	];
 }
 
 export const useRootLoaderData = () => {
-  const [root] = useMatches();
-  return root?.data as SerializeFrom<typeof loader>;
+	const [root] = useMatches();
+	return root?.data as SerializeFrom<typeof loader>;
 };
 
-export async function loader({context}: LoaderFunctionArgs) {
-  const {storefront, session, cart} = context;
-  const customerAccessToken = await session.get('customerAccessToken');
-  const publicStoreDomain = context.env.PUBLIC_STORE_DOMAIN;
+export async function loader({ context }: LoaderFunctionArgs) {
+	const { storefront, session, cart } = context;
+	const customerAccessToken = await session.get("customerAccessToken");
+	const publicStoreDomain = context.env.PUBLIC_STORE_DOMAIN;
 
-  // validate the customer access token is valid
-  const {isLoggedIn, headers} = await validateCustomerAccessToken(
-    session,
-    customerAccessToken,
-  );
+	// validate the customer access token is valid
+	const { isLoggedIn, headers } = await validateCustomerAccessToken(
+		session,
+		customerAccessToken,
+	);
 
-  // defer the cart query by not awaiting it
-  const cartPromise = cart.get();
+	// defer the cart query by not awaiting it
+	const cartPromise = cart.get();
 
-  // defer the footer query (below the fold)
-  const footerPromise = storefront.query(FOOTER_QUERY, {
-    cache: storefront.CacheLong(),
-    variables: {
-      footerMenuHandle: 'footer', // Adjust to your footer menu handle
-    },
-  });
+	// defer the footer query (below the fold)
+	const footerPromise = storefront.query(FOOTER_QUERY, {
+		cache: storefront.CacheLong(),
+		variables: {
+			footerMenuHandle: "footer", // Adjust to your footer menu handle
+		},
+	});
 
-  // await the header query (above the fold)
-  const headerPromise = storefront.query(HEADER_QUERY, {
-    cache: storefront.CacheLong(),
-    variables: {
-      headerMenuHandle: 'main-menu', // Adjust to your header menu handle
-    },
-  });
+	// await the header query (above the fold)
+	const headerPromise = storefront.query(HEADER_QUERY, {
+		cache: storefront.CacheLong(),
+		variables: {
+			headerMenuHandle: "main-menu", // Adjust to your header menu handle
+		},
+	});
 
-  return defer(
-    {
-      cart: cartPromise,
-      footer: footerPromise,
-      header: await headerPromise,
-      isLoggedIn,
-      publicStoreDomain,
-    },
-    {headers},
-  );
+	return defer(
+		{
+			cart: cartPromise,
+			footer: footerPromise,
+			header: await headerPromise,
+			isLoggedIn,
+			publicStoreDomain,
+		},
+		{ headers },
+	);
 }
 
 export default function App() {
-  const nonce = useNonce();
-  const data = useLoaderData<typeof loader>();
+	const nonce = useNonce();
+	const data = useLoaderData<typeof loader>();
 
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Layout {...data}>
-          <Outlet />
-        </Layout>
-        <ScrollRestoration nonce={nonce} />
-        <Scripts nonce={nonce} />
-        <LiveReload nonce={nonce} />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<NextUIProvider>
+					<main className="purple-dark text-foreground bg-background">
+						<Layout {...data}>
+							<Outlet />
+						</Layout>
+						<ScrollRestoration nonce={nonce} />
+						<Scripts nonce={nonce} />
+						<LiveReload nonce={nonce} />
+					</main>
+				</NextUIProvider>
+			</body>
+		</html>
+	);
 }
 
 export function ErrorBoundary() {
-  const error = useRouteError();
-  const rootData = useRootLoaderData();
-  const nonce = useNonce();
-  let errorMessage = 'Unknown error';
-  let errorStatus = 500;
+	const error = useRouteError();
+	const rootData = useRootLoaderData();
+	const nonce = useNonce();
+	let errorMessage = "Unknown error";
+	let errorStatus = 500;
 
-  if (isRouteErrorResponse(error)) {
-    errorMessage = error?.data?.message ?? error.data;
-    errorStatus = error.status;
-  } else if (error instanceof Error) {
-    errorMessage = error.message;
-  }
+	if (isRouteErrorResponse(error)) {
+		errorMessage = error?.data?.message ?? error.data;
+		errorStatus = error.status;
+	} else if (error instanceof Error) {
+		errorMessage = error.message;
+	}
 
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Layout {...rootData}>
-          <div className="route-error">
-            <h1>Oops</h1>
-            <h2>{errorStatus}</h2>
-            {errorMessage && (
-              <fieldset>
-                <pre>{errorMessage}</pre>
-              </fieldset>
-            )}
-          </div>
-        </Layout>
-        <ScrollRestoration nonce={nonce} />
-        <Scripts nonce={nonce} />
-        <LiveReload nonce={nonce} />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Layout {...rootData}>
+					<div className="route-error">
+						<h1>Oops</h1>
+						<h2>{errorStatus}</h2>
+						{errorMessage && (
+							<fieldset>
+								<pre>{errorMessage}</pre>
+							</fieldset>
+						)}
+					</div>
+				</Layout>
+				<ScrollRestoration nonce={nonce} />
+				<Scripts nonce={nonce} />
+				<LiveReload nonce={nonce} />
+			</body>
+		</html>
+	);
 }
 
 /**
@@ -186,27 +193,27 @@ export function ErrorBoundary() {
  * ```
  */
 async function validateCustomerAccessToken(
-  session: LoaderFunctionArgs['context']['session'],
-  customerAccessToken?: CustomerAccessToken,
+	session: LoaderFunctionArgs["context"]["session"],
+	customerAccessToken?: CustomerAccessToken,
 ) {
-  let isLoggedIn = false;
-  const headers = new Headers();
-  if (!customerAccessToken?.accessToken || !customerAccessToken?.expiresAt) {
-    return {isLoggedIn, headers};
-  }
+	let isLoggedIn = false;
+	const headers = new Headers();
+	if (!customerAccessToken?.accessToken || !customerAccessToken?.expiresAt) {
+		return { isLoggedIn, headers };
+	}
 
-  const expiresAt = new Date(customerAccessToken.expiresAt).getTime();
-  const dateNow = Date.now();
-  const customerAccessTokenExpired = expiresAt < dateNow;
+	const expiresAt = new Date(customerAccessToken.expiresAt).getTime();
+	const dateNow = Date.now();
+	const customerAccessTokenExpired = expiresAt < dateNow;
 
-  if (customerAccessTokenExpired) {
-    session.unset('customerAccessToken');
-    headers.append('Set-Cookie', await session.commit());
-  } else {
-    isLoggedIn = true;
-  }
+	if (customerAccessTokenExpired) {
+		session.unset("customerAccessToken");
+		headers.append("Set-Cookie", await session.commit());
+	} else {
+		isLoggedIn = true;
+	}
 
-  return {isLoggedIn, headers};
+	return { isLoggedIn, headers };
 }
 
 const MENU_FRAGMENT = `#graphql
